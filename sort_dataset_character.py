7786ef0f491e8f877fc2febeb040a090c0c3c6c9
WenzhuoXu <xuwenzhuo46@gmail.com>
2024-02-25 16:52:24 -0500

    add dataset sort based on fft

---
+++ sort_dataset_character.py
@@ -0,0 +1,50 @@
+from dataset.MatDataset import BurgersDatasetWhole
+import torch
+from models.teecnet import TEECNetConv
+import matplotlib.pyplot as plt
+
+
+model = TEECNetConv(1, 32, 1, num_layers=6, retrieve_weights=False, num_powers=3)
+dataset = BurgersDatasetWhole(root='data/burgers')
+
+fft_x_list = []
+fft_y_list = []
+
+sub_x_total = []
+sub_y_total = []
+
+for data in dataset:
+    sub_x_list, sub_y_list = model.get_partition_domain(data[0], mode='train'), model.get_partition_domain(data[1], mode='test')
+    sub_x_total.append(sub_x_list)
+    sub_y_total.append(sub_y_list)
+    for sub_x, sub_y in zip(sub_x_list, sub_y_list):
+        # calculate fft of sub_x and sub_y
+        sub_x_fft = torch.fft.fftn(sub_x)
+        sub_y_fft = torch.fft.fftn(sub_y)
+
+        # calculate the dominant frequencies
+        sub_x_fft_abs = torch.abs(sub_x_fft)
+        sub_y_fft_abs = torch.abs(sub_y_fft)
+        sub_x_fft_abs_max = torch.max(sub_x_fft_abs)
+        sub_y_fft_abs_max = torch.max(sub_y_fft_abs)
+
+        # store the dominant frequencies
+        sub_x_fft_abs_max = sub_x_fft_abs_max.item()
+        sub_y_fft_abs_max = sub_y_fft_abs_max.item()
+        fft_x_list.append(sub_x_fft_abs_max)
+        fft_y_list.append(sub_y_fft_abs_max)
+
+
+# save the sub_x_total and sub_y_total lists and the fft_x_list and fft_y_list lists
+torch.save(sub_x_total, 'sub_x_total.pt')
+torch.save(sub_y_total, 'sub_y_total.pt')
+torch.save(fft_x_list, 'fft_x_list.pt')
+torch.save(fft_y_list, 'fft_y_list.pt')
+
+# plot the distribution of the dominant frequencies of the input and output
+plt.hist(fft_x_list, bins=100, alpha=0.5, label='input')
+plt.hist(fft_y_list, bins=100, alpha=0.5, label='output')
+
+
+
+        

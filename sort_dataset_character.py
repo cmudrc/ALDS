6eabb42f915a3c3ea4351c6635da154db780b238
Wenzhuo Xu <wxu2@andrew.cmu.edu>
2024-02-27 16:44:42 -0500

    sort data graph

--- sort_dataset_character.py
+++ sort_dataset_character.py
@@ -3,50 +3,37 @@ import torch
 from models.teecnet import TEECNetConv
 import matplotlib.pyplot as plt
 import tqdm
-
-model = TEECNetConv(1, 32, 1, num_layers=6, retrieve_weights=False, num_powers=3, sub_size=9)
-dataset = BurgersDataset(root='data/burgers')
-
-fft_x_list = []
-fft_y_list = []
-
-sub_x_total = []
-sub_y_total = []
-
-for data in tqdm.tqdm(dataset):
-    data[0] = torch.tensor(data[0], dtype=torch.float)
-    data[1] = torch.tensor(data[1], dtype=torch.float)
-    sub_x_list, sub_y_list = model.get_partition_domain(data[0], mode='train'), model.get_partition_domain(data[1], mode='test')
-    sub_x_total.append(sub_x_list)
-    sub_y_total.append(sub_y_list)
-    for sub_x, sub_y in zip(sub_x_list, sub_y_list):
-        # calculate fft of sub_x and sub_y
-        sub_x_fft = torch.fft.fftn(sub_x)
-        sub_y_fft = torch.fft.fftn(sub_y)
-
-        # calculate the dominant frequencies
-        sub_x_fft_abs = torch.abs(sub_x_fft)
-        sub_y_fft_abs = torch.abs(sub_y_fft)
-        sub_x_fft_abs_max = torch.max(sub_x_fft_abs)
-        sub_y_fft_abs_max = torch.max(sub_y_fft_abs)
-
-        # store the dominant frequencies
-        sub_x_fft_abs_max = sub_x_fft_abs_max.item()
-        sub_y_fft_abs_max = sub_y_fft_abs_max.item()
-        fft_x_list.append(sub_x_fft_abs_max)
-        fft_y_list.append(sub_y_fft_abs_max)
+import numpy as np
+import os
 
 
-# save the sub_x_total and sub_y_total lists and the fft_x_list and fft_y_list lists
-torch.save(sub_x_total, 'sub_x_total.pt')
-torch.save(sub_y_total, 'sub_y_total.pt')
-torch.save(fft_x_list, 'fft_x_list.pt')
-torch.save(fft_y_list, 'fft_y_list.pt')
+def plot_prediction(window_size, y, y_pred, epoch, batch_idx, folder):
+    xx, yy = np.meshgrid(np.linspace(0, 1, window_size), np.linspace(0, 1, window_size))
+    fig, axs = plt.subplots(1, 3, figsize=(17, 5))
+    axs[0].contourf(xx, yy, y.cpu().detach().numpy().reshape(window_size, window_size), levels=100, cmap='plasma')
+    axs[0].set_title('(a) Ground truth')
+    axs[0].axis('off')
+    axs[1].contourf(xx, yy, y_pred.reshape(window_size, window_size), levels=100, cmap='plasma')
+    axs[1].set_title('(b) Prediction')
+    axs[1].axis('off')
+    axs[2].contourf(xx, yy, np.abs(y.cpu().detach().numpy().reshape(window_size, window_size) - y_pred.reshape(window_size, window_size)), levels=100, cmap='plasma')
+    axs[2].set_title('(c) Absolute difference')
+    axs[2].axis('off')
 
-# plot the distribution of the dominant frequencies of the input and output
-plt.hist(fft_x_list, bins=100, alpha=0.5, label='input')
-plt.hist(fft_y_list, bins=100, alpha=0.5, label='output')
+    # plt.colorbar(axs[2].contourf(xx, yy, np.abs(y.cpu().detach().numpy().reshape(window_size, window_size) - y_pred.reshape(window_size, window_size)), levels=100, cmap='plasma'), ax=axs[2], pad=0.01)
+
+    plt.savefig(os.path.join(folder, f'epoch_{epoch}_batch_{batch_idx}.png'))
+    # wandb.log({'prediction': wandb.Image(plt)})
+    plt.close()
 
 
+sub_x_total = torch.load('sub_x_total.pt')
+# sub_y_total = torch.load('sub_y_total.pt')
+fft_x_list = torch.load('fft_x_list.pt')
 
+sub_x_total = torch.tensor(sub_x_total, dtype=torch.float).squeeze()
+fft_max = torch.max(fft_x_list)
+
+print('fft_max: {}'.format(fft_max))
+print(fft_max.shape)
         

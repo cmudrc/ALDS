140da38ff3f986b1e929d17ae1d48f2a4b8af4af
WenzhuoXu <xuwenzhuo46@gmail.com>
2025-01-06 16:45:22 -0500

    enable multi-gpu tuning

--- models/scheduler_gnn.py
+++ models/scheduler_gnn.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 # import torch
 from torch.utils.data import random_split
 import torch.nn as nn
+import torch.distributed as dist
+import torch.multiprocessing as mp
 # import torch_geometric as pyg
 # import torch_geometric.nn as pyg_nn    
 from torch_geometric.loader import DataLoader
@@ -34,7 +36,7 @@ class GNNPartitionScheduler():
     def get_sub_dataset(self):
         return self.subsets
     
-    def _initialize_model(self, model_type, in_channels, out_channels, **kwargs):
+    def _initialize_model(self):
         return self.model
     
     def _load_models(self):
@@ -77,84 +79,95 @@ class GNNPartitionScheduler():
 
         return subsets
     
-    def _train_sub_models(self, train_config, device, subset_idx=None, is_parallel=False):
+    def _train_sub_models(self, model, train_config, device, subset_idx=None, is_parallel=False):
         models = []
         if subset_idx is not None:
             subsets = [self.subsets[idx] for idx in subset_idx]
         else:
             subsets = self.subsets
-        for i, subset in enumerate(subsets):
-            # print(len(subset))
-            wandb.init(project='domain_partition_scheduler', group='partition_training', config=train_config)
-            train_dataset, val_dataset = random_split(subset, [int(0.8 * len(subset)), len(subset) - int(0.8 * len(subset))])
-            # print(train_config['batch_size'])
-            train_loader = DataLoader(train_dataset, batch_size=train_config['batch_size'], shuffle=True)
-            val_loader = DataLoader(val_dataset, batch_size=train_config['batch_size'], shuffle=False)
-            model = self._initialize_model(self.model, 8, 8, width=64)
 
-            # model = model.to(device)
-            if is_parallel:
-                model = nn.DataParallel(model)
+        if is_parallel:
+            world_size = torch.cuda.device_count()
+            mp.spawn(
+                self._train_sub_models_parallel,
+                args=(self.model, self.name, world_size, subsets, train_config),
+                nprocs=world_size,
+                join=True,
+            )
+
+        else:
+            for i, subset in enumerate(subsets):
+                # print(len(subset))
+                wandb.init(project='domain_partition_scheduler', group='partition_training', config=train_config)
+                train_dataset, val_dataset = random_split(subset, [int(0.8 * len(subset)), len(subset) - int(0.8 * len(subset))])
+                # print(train_config['batch_size'])
+                train_loader = DataLoader(train_dataset, batch_size=train_config['batch_size'], shuffle=True)
+                val_loader = DataLoader(val_dataset, batch_size=train_config['batch_size'], shuffle=False)
+                # model = self._initialize_model(self.model, 8, 8, width=64)
+
                 model = model.to(device)
-            else:
-                model = model.to(device)
+                # if is_parallel:
+                #     model = nn.DistributedDataParallel(model, device_ids=[device])
+                #     model = model.to(device)
+                # else:
+                #     model = model.to(device)
 
-            criterion = torch.nn.MSELoss()
-            optimizer = torch.optim.Adam(model.parameters(), lr=train_config['lr'])
-            scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=train_config['step_size'], gamma=train_config['gamma'])
-            epochs = train_config['epochs']
-            log_interval = train_config['log_interval']
-            val_interval = train_config['val_interval']
-            best_loss = np.inf
-            for epoch in range(epochs):
-                model.train()
-                train_loss = 0
-                for batch in train_loader:
-                    optimizer.zero_grad()
-                    batch = batch.to(device)
-                    out = model(batch.x, batch.edge_index, batch.edge_attr)
-                    loss = criterion(out, batch.y)
-                    loss.backward()
-                    optimizer.step()
-                    train_loss += loss.item()
-                train_loss /= len(train_loader)
-                wandb.log({'train_loss': train_loss})
-                if epoch % log_interval == 0:
-                    print(f'Epoch {epoch}: Train loss: {train_loss}')
-                if epoch % val_interval == 0:
-                    model.eval()
-                    val_loss = 0
-                    with torch.no_grad():
-                        for batch in val_loader:
-                            batch = batch.to(device)
-                            out = model(batch.x, batch.edge_index, batch.edge_attr)
-                            loss = criterion(out, batch.y)
-                            val_loss += loss.item()
-                        val_loss /= len(val_loader)
-                        wandb.log({'val_loss': val_loss})
-                        plot_3d_prediction(batch[0], out, save_mode='wandb', path='logs/figures/{}'.format(self.name))
-                        if val_loss < best_loss:
-                            best_loss = val_loss
-                            os.makedirs('logs/models/collection_{}'.format(self.name), exist_ok=True)
-                            torch.save(model.state_dict(), 'logs/models/collection_{}/partition_{}.pth'.format(self.name, i))
-                            print(f'Epoch {epoch}: Validation loss: {val_loss}')
-                scheduler.step()
+                criterion = torch.nn.MSELoss()
+                optimizer = torch.optim.Adam(model.parameters(), lr=train_config['lr'])
+                scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=train_config['step_size'], gamma=train_config['gamma'])
+                epochs = train_config['epochs']
+                log_interval = train_config['log_interval']
+                val_interval = train_config['val_interval']
+                best_loss = np.inf
+                for epoch in range(epochs):
+                    model.train()
+                    train_loss = 0
+                    for batch in train_loader:
+                        optimizer.zero_grad()
+                        batch = batch.to(device)
+                        out = model(batch.x, batch.edge_index, batch.edge_attr)
+                        loss = criterion(out, batch.y)
+                        loss.backward()
+                        optimizer.step()
+                        train_loss += loss.item()
+                    train_loss /= len(train_loader)
+                    wandb.log({'train_loss': train_loss})
+                    if epoch % log_interval == 0:
+                        print(f'Epoch {epoch}: Train loss: {train_loss}')
+                    if epoch % val_interval == 0:
+                        model.eval()
+                        val_loss = 0
+                        with torch.no_grad():
+                            for batch in val_loader:
+                                batch = batch.to(device)
+                                out = model(batch.x, batch.edge_index, batch.edge_attr)
+                                loss = criterion(out, batch.y)
+                                val_loss += loss.item()
+                            val_loss /= len(val_loader)
+                            wandb.log({'val_loss': val_loss})
+                            plot_3d_prediction(batch[0], out, save_mode='wandb', path='logs/figures/{}'.format(self.name))
+                            if val_loss < best_loss:
+                                best_loss = val_loss
+                                os.makedirs('logs/models/collection_{}'.format(self.name), exist_ok=True)
+                                torch.save(model.state_dict(), 'logs/models/collection_{}/partition_{}.pth'.format(self.name, i))
+                                print(f'Epoch {epoch}: Validation loss: {val_loss}')
+                    scheduler.step()
 
-            models.append(model)
-            wandb.finish()
+                models.append(model)
+                wandb.finish()
         return models
     
     def train(self, train_config, subset_idx=None):
         # for parallel training on multiple gpus
         if torch.cuda.device_count() > 1:
             print(f'Using {torch.cuda.device_count()} GPUs')
-            self._train_sub_models(train_config, torch.device('cuda'), subset_idx, is_parallel=True)
+            self._train_sub_models(self.model, train_config, torch.device('cuda'), subset_idx, is_parallel=True)
         elif torch.cuda.device_count() == 1:
             print('Using single GPU')
-            self._train_sub_models(train_config, torch.device('cuda'), subset_idx, is_parallel=False)
+            self._train_sub_models(self.model, train_config, torch.device('cuda'), subset_idx, is_parallel=False)
         else:
             print('Using CPU')
-            self._train_sub_models(train_config, torch.device('cpu'), subset_idx, is_parallel=False)
+            self._train_sub_models(self.model, train_config, torch.device('cpu'), subset_idx, is_parallel=False)
 
     def predict(self, x):
         # pred_y_list = []
@@ -166,3 +179,80 @@ class GNNPartitionScheduler():
                     x[i].x = pred_y
         return x
                  
+    @staticmethod
+    def _train_sub_models_parallel(model, name, rank, world_size, subsets, train_config):
+        # Setup distributed process group
+        os.environ['MASTER_ADDR'] = 'localhost'
+        os.environ['MASTER_PORT'] = '12355'
+        dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
+        local_device = f"cuda:{rank}"
+        models = []
+
+        # Iterate through subsets
+        for i, subset in enumerate(subsets):
+            # Split dataset
+            train_dataset, val_dataset = random_split(
+                subset, 
+                [int(0.8 * len(subset)), len(subset) - int(0.8 * len(subset))]
+            )
+            train_loader = DataLoader(
+                train_dataset, batch_size=train_config['batch_size'], shuffle=True, num_workers=4
+            )
+            val_loader = DataLoader(
+                val_dataset, batch_size=train_config['batch_size'], shuffle=False, num_workers=4
+            )
+
+            # Initialize model and wrap it with DistributedDataParallel
+            # model = self._initialize_model(self.model, 8, 8, width=64)
+            model = model.to(local_device)
+            model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[rank])
+
+            # Set up optimizer, criterion, and scheduler
+            criterion = torch.nn.MSELoss()
+            optimizer = torch.optim.Adam(model.parameters(), lr=train_config['lr'])
+            scheduler = torch.optim.lr_scheduler.StepLR(
+                optimizer, step_size=train_config['step_size'], gamma=train_config['gamma']
+            )
+
+            # Training loop
+            best_loss = np.inf
+            for epoch in range(train_config['epochs']):
+                model.train()
+                train_loss = 0
+                for batch in train_loader:
+                    optimizer.zero_grad()
+                    batch = batch.to(local_device)
+                    out = model(batch.x, batch.edge_index, batch.edge_attr)
+                    loss = criterion(out, batch.y)
+                    loss.backward()
+                    optimizer.step()
+                    train_loss += loss.item()
+                train_loss /= len(train_loader)
+
+                # Validation loop
+                if epoch % train_config['val_interval'] == 0:
+                    model.eval()
+                    val_loss = 0
+                    with torch.no_grad():
+                        for batch in val_loader:
+                            batch = batch.to(local_device)
+                            out = model(batch.x, batch.edge_index, batch.edge_attr)
+                            loss = criterion(out, batch.y)
+                            val_loss += loss.item()
+                        val_loss /= len(val_loader)
+
+                        # Save the best model
+                        if val_loss < best_loss:
+                            best_loss = val_loss
+                            os.makedirs(f'logs/models/collection_{name}', exist_ok=True)
+                            torch.save(
+                                model.state_dict(), 
+                                f'logs/models/collection_{name}/partition_{i}.pth'
+                            )
+
+            models.append(model)
+
+        # Cleanup distributed process group
+        dist.destroy_process_group()
+        return models
